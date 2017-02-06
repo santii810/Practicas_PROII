241a91f589903bbba60ba18e9935cbfd40dcb096
santii810 <santii810@hotmail.com>
2017-02-06 09:44:15 +0100

    Practica 1 ejercicio2

---
+++ Practica1/P1_Ej2/src/p1_ej2/DireccionCorreo.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p1_ej2;
+
+/**
+ *
+ * @author santi
+ */
+public class DireccionCorreo {
+
+    private final String DEFAULT_SERVER = "esei.uvigo.es";
+    private String nombre;
+    private String apellido;
+    private String usuario;
+    private String servidor;
+
+    public DireccionCorreo(String nombre, String apellido, String usuario, String servidor) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.usuario = usuario;
+        this.servidor = servidor;
+    }
+
+    public DireccionCorreo(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.usuario = apellido + nombre.substring(0, 1);
+        this.servidor = DEFAULT_SERVER;
+    }
+
+    @Override
+    public String toString() {
+        return "DireccionCorreo{" + "nombre=" + nombre + ", apellido=" + apellido + ", usuario=" + usuario + ", servidor=" + servidor + '}';
+    }
+
+}
---
+++ Practica1/P1_Ej2/src/p1_ej2/P1_Ej2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p1_ej2;
+
+/**
+ *
+ * @author santi
+ */
+public class P1_Ej2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        DireccionCorreo dir = new DireccionCorreo("Santiago", "Gomez");
+        System.out.println(dir.toString());
+
+    }
+
+}
