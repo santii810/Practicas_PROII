0cd8a86647eb732566a95b7ba7991fb2b8d01aa1
santii810 <santii810@hotmail.com>
2017-02-07 09:49:54 +0100

    Practica1 ejercicio 4

---
+++ Practica1/P1_Ej4/src/p1_ej4/P1_Ej4.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p1_ej4;
+
+/**
+ *
+ * Un polígono regular es una figura que está formada por un número de lados numLados. 
+ * Los polígonos se expresan como "Polígono de lados: numLados y angulos:numAngulos". 
+ * Para calcular los ángulos entre los lados del polígono, se divide 360 entre el número de lados. 
+ * El polígono regular más pequeño posible es el de tres lados (un triángulo equilátero).
+ * 
+ * @author santi
+ */
+public class P1_Ej4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        Poligono poli = new Poligono(4, 5);
+    }
+    
+}
---
+++ Practica1/P1_Ej4/src/p1_ej4/Poligono.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p1_ej4;
+
+/**
+ *
+ * @author santi
+ */
+public class Poligono {
+
+    private static final int MIN_LADOS  = 3;
+    private int numLados;
+    private int numAngulos;
+
+    public Poligono(int numLados, int numAngulos) {
+        this.numLados = numLados;
+        this.numAngulos = numAngulos;
+    }
+
+}
