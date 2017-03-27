e48f8d0a5a89bbece16332321fd5316d12866a0c
santii810 <santii810@hotmail.com>
2017-03-27 13:14:13 +0200

    Proyecto2 Ejercicio 2

--- Proyecto2/Biblio2/src/es/uvigo/esei/pro2/ui/Ilc.java
+++ Proyecto2/Biblio2/src/es/uvigo/esei/pro2/ui/Ilc.java
@@ -1,7 +1,13 @@
 package es.uvigo.esei.pro2.ui;
 
+import es.uvigo.esei.pro2.core.ArticuloRevista;
 import es.uvigo.esei.pro2.core.Bibliografia;
+import es.uvigo.esei.pro2.core.DocumentoWeb;
+import es.uvigo.esei.pro2.core.Fecha;
+import es.uvigo.esei.pro2.core.Libro;
+import es.uvigo.esei.pro2.core.Libro.TipoFormato;
 import es.uvigo.esei.pro2.core.Referencia;
+import java.util.Date;
 
 import java.util.Scanner;
 
@@ -13,6 +19,8 @@ public class Ilc {
     /**
      * Realiza el reparto de la funcionalidad ler = lee, evalua, repite
      */
+    private static Scanner teclado = new Scanner(System.in);
+
     public void ler() {
         int op;
 
@@ -65,7 +73,6 @@ public class Ilc {
     private int leeNum(String msg) {
         boolean repite;
         int toret = 0;
-        Scanner teclado = new Scanner(System.in);
 
         do {
             repite = false;
@@ -113,7 +120,24 @@ public class Ilc {
      * @param coleccion La coleccion en la que se inserta la referencia.
      */
     private void insertaReferencia(Bibliografia coleccion) throws Exception {
-        Referencia r = new Referencia("", "", 0, Referencia.TipoReferencia.LIBRO);
+        Referencia r = null;
+        char tipoReferencia;
+
+        do {
+            tipoReferencia = leeCaracter("Introduce el tipo de referencia (L: libro, A: artículo revista, D: documento web): ");
+        } while ((tipoReferencia != 'L') && (tipoReferencia != 'A') && (tipoReferencia != 'D'));
+
+        switch (tipoReferencia) {
+            case 'L':
+                r = new Libro("", "", 0, Referencia.TipoReferencia.LIBRO);
+                break;
+            case 'A':
+                r = new ArticuloRevista("", "", 0, Referencia.TipoReferencia.ARTICULOREVISTA);
+                break;
+            case 'D':
+                r = new DocumentoWeb("", "", 0, Referencia.TipoReferencia.DOCUMENTOWEB);
+                break;
+        }
 
         modificaReferencia(r);
         coleccion.inserta(r);
@@ -143,9 +167,7 @@ public class Ilc {
 
     private void modificaReferencia(Referencia r) {
         String info;
-        char tipoReferencia;
-        Scanner teclado = new Scanner(System.in);
-        Referencia.TipoReferencia etiqueta;
+        char formato;
 
         // Autores
         System.out.print("Autores de la referencia ");
@@ -183,23 +205,134 @@ public class Ilc {
             r.setAno(Integer.parseInt(info));
         }
 
-        // Tipo de referencia          
-        do {
-            tipoReferencia = leeCaracter("Introduce el tipo de referencia (L: libro, A: artículo revista, D: documento web): ");
-        } while ((tipoReferencia != 'L') && (tipoReferencia != 'A') && (tipoReferencia != 'D'));
+        // Tipos de referencia
+        if (r instanceof Libro) {
 
-        switch (tipoReferencia) {
-            case 'L':
-                r.setTipo(Referencia.TipoReferencia.LIBRO);
-                break;
-            case 'A':
-                r.setTipo(Referencia.TipoReferencia.ARTICULOREVISTA);
-                break;
-            case 'D':
-                r.setTipo(Referencia.TipoReferencia.DOCUMENTOWEB);
-                break;
+// Editorial
+            System.out.print("Editorial del libro ");
+            if (((Libro) r).getEditorial().length() > 0) {
+                System.out.print("[" + ((Libro) r).getEditorial() + "]");
+            }
+            System.out.print(": ");
+            info = teclado.nextLine().trim();
+
+            if (info.length() > 0) {
+                ((Libro) r).setEditorial(info);
+            }
+
+            // Editorial
+            System.out.print("Isbn del libro ");
+            if (((Libro) r).getIsbn().length() > 0) {
+                System.out.print("[" + ((Libro) r).getIsbn() + "]");
+            }
+            System.out.print(": ");
+            info = teclado.nextLine().trim();
+
+            if (info.length() > 0) {
+                ((Libro) r).setIsbn(info);
+            }
+
+            //Formato
+            do {
+                formato = leeCaracter("Introduce el tipo de referencia (P: papel, E: ebook): ");
+            } while ((formato != 'L') && (formato != 'A') && (formato != 'D'));
+
+            switch (formato) {
+                case 'P':
+                    ((Libro) r).setFormato(TipoFormato.PAPEL);
+                    break;
+                case 'E':
+                    ((Libro) r).setFormato(TipoFormato.EBOOK);
+                    break;
+            }
+        } else if (r instanceof ArticuloRevista) {
+            // TipoRevista
+            System.out.print("Tipo de revista ");
+            if (((ArticuloRevista) r).getTipoRevista().length() > 0) {
+                System.out.print("[" + ((ArticuloRevista) r).getTipoRevista() + "]");
+            }
+            System.out.print(": ");
+            info = teclado.nextLine().trim();
+
+            if (info.length() > 0) {
+                ((ArticuloRevista) r).setTipoRevista(info);
+            }
+
+            // doi
+            System.out.print("Doi de la revista ");
+            if (((ArticuloRevista) r).getDoi().length() > 0) {
+                System.out.print("[" + ((ArticuloRevista) r).getDoi() + "]");
+            }
+            System.out.print(": ");
+            info = teclado.nextLine().trim();
+
+            if (info.length() > 0) {
+                ((ArticuloRevista) r).setDoi(info);
+            }
+
+            // Volumen
+            System.out.print("Volumen de la revista ");
+            if (((ArticuloRevista) r).getVolumen() > 0) {
+                System.out.print("[" + ((ArticuloRevista) r).getVolumen() + "]");
+            }
+            System.out.print(": ");
+            info = teclado.nextLine().trim();
+
+            if (info.length() > 0) {
+                ((ArticuloRevista) r).setVolumen(Integer.parseInt(info));
+            }
+
+            // Numero
+            System.out.print("Numero de la revista ");
+            if (((ArticuloRevista) r).getNumero() > 0) {
+                System.out.print("[" + ((ArticuloRevista) r).getNumero() + "]");
+            }
+            System.out.print(": ");
+            info = teclado.nextLine().trim();
+
+            if (info.length() > 0) {
+                ((ArticuloRevista) r).setNumero(Integer.parseInt(info));
+            }
+
+            // PaginaInicio
+            System.out.print("Pagina de inicio de la revista ");
+            if (((ArticuloRevista) r).getPaginaInicio() > 0) {
+                System.out.print("[" + ((ArticuloRevista) r).getPaginaInicio() + "]");
+            }
+            System.out.print(": ");
+            info = teclado.nextLine().trim();
+
+            if (info.length() > 0) {
+                ((ArticuloRevista) r).setPaginaInicio(Integer.parseInt(info));
+            }
+
+            // PaginaFin
+            System.out.print("Pagina de fin de la revista ");
+            if (((ArticuloRevista) r).getPaginaFin() > 0) {
+                System.out.print("[" + ((ArticuloRevista) r).getPaginaFin() + "]");
+            }
+            System.out.print(": ");
+            info = teclado.nextLine().trim();
+
+            if (info.length() > 0) {
+                ((ArticuloRevista) r).setPaginaFin(Integer.parseInt(info));
+            }
+        } else if (r instanceof DocumentoWeb) {
+
+            // Url
+            System.out.print("Url de la web ");
+            if (((DocumentoWeb) r).getUrl().length() > 0) {
+                System.out.print("[" + ((DocumentoWeb) r).getUrl() + "]");
+            }
+            System.out.print(": ");
+            info = teclado.nextLine().trim();
+
+            if (info.length() > 0) {
+                ((DocumentoWeb) r).setUrl(info);
+            }
+
+            ((DocumentoWeb) r).setFechaConsulta(new Fecha(new Date().getDay(), new Date().getMonth(), new Date().getYear()));
         }
-
     }
 
     /**
@@ -224,8 +357,6 @@ public class Ilc {
      * @return el caracter introducido por el usuario
      */
     private char leeCaracter(String men) {
-        Scanner teclado = new Scanner(System.in);
-
         System.out.print(men);
         return (teclado.nextLine().trim().charAt(0));
     }
