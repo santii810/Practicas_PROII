f40c7adeb3c65a524218a3c898ba2b441ac2c48e
Santiago Gómez <santii810@hotmail.com>
2017-05-03 22:32:57 +0200

    creado toDOM para referencia

--- Proyecto3/Biblio3/src/es/uvigo/esei/pro2/core/Referencia.java
+++ Proyecto3/Biblio3/src/es/uvigo/esei/pro2/core/Referencia.java
@@ -1,81 +1,117 @@
 // Definicion de la clase Referencia
-
 package es.uvigo.esei.pro2.core;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import nu.xom.*;
 
 /**
  *
  * @author nrufino
  */
 abstract public class Referencia {
-    private String autores; 
-    private String titulo;  
+
+    private String autores;
+    private String titulo;
     private int ano;
 
-    /** Crea una nueva referencia, con sus autores, título y año
+    /**
+     * Crea una nueva referencia, con sus autores, título y año
+     *
      * @param autores los nombres de los autores de la referencia
      * @param titulo el título de la referencia
      * @param ano el ano de la referencia
      */
-    public Referencia(String autores, String titulo, int ano)
-    {
-        this.setAutores( autores );
-        this.setTitulo( titulo );
+    public Referencia(String autores, String titulo, int ano) {
+        this.setAutores(autores);
+        this.setTitulo(titulo);
         this.setAno(ano);
     }
 
-    /** Devuelve el titulo de la referencia
+    /**
+     * Devuelve el titulo de la referencia
+     *
      * @return el titulo de la referencia, como String.
      */
-    public String getTitulo()
-    {
+    public String getTitulo() {
         return titulo;
     }
 
-    /** Cambia el titulo de la referencia
+    /**
+     * Cambia el titulo de la referencia
+     *
      * @param t el titulo de la referencia
      */
-    public void setTitulo(String t)
-    {
+    public void setTitulo(String t) {
         titulo = t.trim();
     }
 
-    /** Devuelve los autores de la referencia
-     *  @return El valor como cadena
-     **/
+    /**
+     * Devuelve los autores de la referencia
+     *
+     * @return El valor como cadena
+     *
+     */
     public String getAutores() {
         return autores;
     }
 
-    /** Cambia los autores
+    /**
+     * Cambia los autores
+     *
      * @param autores El nuevo valor, como cadena
      */
     public void setAutores(String autores) {
         this.autores = autores.trim();
     }
 
-    /** Devuelve el año de la referencia
-     *  @return El valor como entero
-     **/
+    /**
+     * Devuelve el año de la referencia
+     *
+     * @return El valor como entero
+     *
+     */
     public int getAno() {
         return ano;
     }
 
-    /** Cambia el año
+    /**
+     * Cambia el año
+     *
      * @param ano El nuevo valor, como entero
      */
     public void setAno(int ano) {
         this.ano = ano;
     }
-  
-    public String toString()
-    {
+
+    public String toString() {
         StringBuilder toret = new StringBuilder();
-        
-        toret.append(getAutores() + " ; " );
-        toret.append(getTitulo() + " ; " );
-        toret.append(getAno() + " ; " ); 
+
+        toret.append(getAutores() + " ; ");
+        toret.append(getTitulo() + " ; ");
+        toret.append(getAno() + " ; ");
 
         return toret.toString();
     }
-}
 
+    public void toDOM() {
+        Element raiz = new Element("mensajes");
+        Element mensaje = new Element("mensaje");
+
+        // Crear el documento
+        mensaje.appendChild("Hola, mundo");
+        raiz.appendChild(mensaje);
+        Document doc = new Document(raiz);
+
+        // Guardarlo
+        try {
+            FileOutputStream f = new FileOutputStream("mensaje.xml");
+            Serializer serial = new Serializer(f);
+            serial.write(doc);
+            f.close();
+            System.out.println("Ok");
+        } catch (IOException exc) {
+            System.err.println("ERROR de archivo: " + exc.getMessage());
+        }
+    }
+}
